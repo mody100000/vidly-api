9788fbf25b89cd7236e28149419d1721347b0883
mody100000 <m.anamody10000@gmail.com>
2022-06-18 04:54:04 +0200

    commit

--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,7 @@ const { Movie, validate } = require("../models/movie");
 const { Genre } = require("../models/genre");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
+const validateObjectId = require("../middleware/validateObjectId");
 const moment = require("moment");
 const mongoose = require("mongoose");
 const express = require("express");
@@ -14,7 +15,7 @@ router.get("/", async (req, res) => {
   res.send(movies);
 });
 
-router.post("/", [auth, admin], async (req, res) => {
+router.post("/", [auth], async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -36,7 +37,7 @@ router.post("/", [auth, admin], async (req, res) => {
   res.send(movie);
 });
 
-router.put("/:id", [auth, admin], async (req, res) => {
+router.put("/:id", [auth], async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -72,7 +73,7 @@ router.delete("/:id", [auth, admin], async (req, res) => {
   res.send(movie);
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", validateObjectId, async (req, res) => {
   const movie = await Movie.findById(req.params.id).select("-__v");
 
   if (!movie)
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const auth = require("../middleware/auth");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const _ = require("lodash");
 const { User, validate } = require("../models/user");
 const express = require("express");
@@ -25,6 +25,7 @@ router.post("/", async (req, res) => {
   const token = user.generateAuthToken();
   res
     .header("x-auth-token", token)
+    .header("access-control-expose-headers", "x-auth-token")
     .send(_.pick(user, ["_id", "name", "email"]));
 });
 
